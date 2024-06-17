734f1371253949cdab382395b8bf175bb731eb7d
joao-zanutto <jpberno@gmail.com>
2024-06-16 21:44:37 -0300

    add infinite loop

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -8,10 +8,11 @@ import (
 )
 
 func main() {
-	// Uncomment this block to pass the first stage
-	fmt.Fprint(os.Stdout, "$ ")
+	for {
+		fmt.Fprint(os.Stdout, "$ ")
 
-	// Wait for user input
-	input, _ := bufio.NewReader(os.Stdin).ReadString('\n')
-	fmt.Fprint(os.Stdout, input[:len(input)-1]+": command not found\n")
+		// Wait for user input
+		input, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+		fmt.Fprint(os.Stdout, input[:len(input)-1]+": command not found\n")
+	}
 }

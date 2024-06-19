87e916d377dfbe1ab3056c8c604cf2966a358f72
joao-zanutto <jpberno@gmail.com>
2024-06-19 20:45:17 -0300

    add type command

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -4,32 +4,54 @@ import (
 	"bufio"
 	"strconv"
 
-	// Uncomment this block to pass the first stage
 	"fmt"
 	"os"
 
 	"strings"
 )
 
+func exit(input []string) {
+	exit_code, _ := strconv.Atoi(input[0])
+	os.Exit(exit_code)
+}
+
+func echo(input []string) {
+	fmt.Println(input[0])
+}
+
+func getType(input []string, c map[string]func([]string)) {
+	if c[input[0]] != nil || input[0] == "type" {
+		fmt.Println(input[0] + " is a shell builtin")
+	} else {
+		fmt.Println(input[0] + ": not found")
+	}
+}
+
 func main() {
+	c := map[string]func([]string){
+		"echo": echo,
+		"exit": exit,
+	}
+
 	for {
 		fmt.Fprint(os.Stdout, "$ ")
 
-		// Get user input, split it based on whitespaces and trim newline from last element
+		// Read input,
 		input, _ := bufio.NewReader(os.Stdin).ReadString('\n')
 		cmd := strings.Split(input, " ")
 		cmd[len(cmd)-1] = strings.TrimRight(cmd[len(cmd)-1], "\n")
 
-		// Run bult-in commands
-		if cmd[0] == "exit" {
-			exit_code, _ := strconv.Atoi(cmd[1])
-			os.Exit(exit_code)
-		}
-		if cmd[0] == "echo" {
-			fmt.Fprint(os.Stdout, strings.Join(cmd[1:], " ")+"\n")
-			continue
+		if c[cmd[0]] == nil {
+			if(cmd[0]) != "type" {
+				fmt.Println(cmd[0] + ": command not found")
+				continue
+			}
 		}
 
-		fmt.Fprint(os.Stdout, input[:len(input)-1]+": command not found\n")
+		switch cmd[0]{
+		case "type": getType(cmd[1:], c)
+		default: c[cmd[0]](cmd[1:])
+		}
+
 	}
 }

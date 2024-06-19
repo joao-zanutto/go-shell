6382ca23ec60b099c55f3dcaa4ddc78b4d77ce11
joao-zanutto <jpberno@gmail.com>
2024-06-19 18:48:54 -0300

    add echo

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -15,16 +15,21 @@ func main() {
 	for {
 		fmt.Fprint(os.Stdout, "$ ")
 
-		// Wait for user input
+		// Get user input, split it based on whitespaces and trim newline from last element
 		input, _ := bufio.NewReader(os.Stdin).ReadString('\n')
-
 		cmd := strings.Split(input, " ")
+		cmd[len(cmd)-1] = strings.TrimRight(cmd[len(cmd)-1], "\n")
+
+		// Run bult-in commands
 		if cmd[0] == "exit" {
 			exit_code, _ := strconv.Atoi(cmd[1])
 			os.Exit(exit_code)
 		}
+		if cmd[0] == "echo" {
+			fmt.Fprint(os.Stdout, strings.Join(cmd[1:], " "))
+			continue
+		}
 
 		fmt.Fprint(os.Stdout, input[:len(input)-1]+": command not found\n")
-
 	}
 }

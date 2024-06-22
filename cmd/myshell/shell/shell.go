b3315ff62a3d8b53906116993e06e76e9a486b24
joao-zanutto <jpberno@gmail.com>
2024-06-22 20:04:33 -0300

    redirect output and error

--- cmd/myshell/shell/shell.go
+++ cmd/myshell/shell/shell.go
@@ -12,43 +12,43 @@ type shell struct {
 	pc map[string]string
 }
 
-func (s shell) exit(input []string) {
+func (s shell) exit(args []string) {
 	os.Exit(0)
 }
 
-func (s shell) echo(input []string) {
-	fmt.Println(strings.Join(input, " "))
+func (s shell) echo(args []string) {
+	fmt.Println(strings.Join(args, " "))
 }
 
-func (s shell) getType(input []string) {
-	if s.c[input[0]] != nil {
-		fmt.Println(input[0] + " is a shell builtin")
-	} else if s.pc[input[0]] != "" {
-		fmt.Println(input[0] + " is " + s.pc[input[0]] + "/" + input[0])
+func (s shell) getType(args []string) {
+	if s.c[args[0]] != nil {
+		fmt.Println(args[0] + " is a shell builtin")
+	} else if s.pc[args[0]] != "" {
+		fmt.Println(args[0] + " is " + s.pc[args[0]] + "/" + args[0])
+	} else {
+		fmt.Println(args[0] + ": not found")
 	}
-	fmt.Println(input[0] + ": not found")
 }
 
-func (s shell) Execute(command string, input []string) {
+func (s shell) Execute(command string, args []string) {
 	if s.c[command] != nil {
-		s.c[command](input)
-	}
-	cmd := exec.Command(s.pc[command] + "/" + command, input...)
-	err := cmd.Run()
-	if err != nil {
-		fmt.Println(command + ": command not found")
+		s.c[command](args)
+	} else {
+		cmd := exec.Command(command, args...)
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		err := cmd.Run()
+		if err != nil {
+			fmt.Println(command + ": command not found")
+		}
 	}
 }
 
 func New() shell {
 	s:= shell { c: make(map[string]func([]string)), pc: make(map[string]string) }
-
-	// Load builtins
 	s.c["echo"] = s.echo
 	s.c["exit"] = s.exit
 	s.c["type"] = s.getType
-
-	// Load programs from PATH
 	dirs := strings.Split(os.Getenv("PATH"), ":")
 	for _, dir := range dirs {
 		files, _ := os.ReadDir(dir)

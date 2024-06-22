ea830bb32f8c242ed685262b3c81a2ee1a23a034
joao-zanutto <jpberno@gmail.com>
2024-06-22 19:39:12 -0300

    add run command

--- cmd/myshell/shell/shell.go
+++ cmd/myshell/shell/shell.go
@@ -2,8 +2,8 @@ package shell
 
 import (
 	"fmt"
-	"strconv"
 	"os"
+	"os/exec"
 	"strings"
 )
 
@@ -13,11 +13,6 @@ type shell struct {
 }
 
 func (s shell) exit(input []string) {
-	if len(input) > 0 {
-		exit_code, _ := strconv.Atoi(input[0])
-		os.Exit(exit_code)
-	}
-
 	os.Exit(0)
 }
 
@@ -25,24 +20,35 @@ func (s shell) echo(input []string) {
 	fmt.Println(strings.Join(input, " "))
 }
 
-func (s shell) isBuiltin(command string) bool{
-	if s.c[command] != nil {
-		return true
-	}
-	return false
-}
-
 func (s shell) getType(input []string) {
-	if s.isBuiltin(input[0]) {
+	if s.c[input[0]] != nil {
 		fmt.Println(input[0] + " is a shell builtin")
 	} else if s.pc[input[0]] != "" {
 		fmt.Println(input[0] + " is " + s.pc[input[0]] + "/" + input[0])
-	} else{
-		fmt.Println(input[0] + ": not found")
+	}
+	fmt.Println(input[0] + ": not found")
+}
+
+func (s shell) Execute(command string, input []string) {
+	if s.c[command] != nil {
+		s.c[command](input)
+	}
+	cmd := exec.Command(s.pc[command] + "/" + command, input...)
+	err := cmd.Run()
+	if err != nil {
+		fmt.Println(command + ": command not found")
 	}
 }
 
-func (s *shell) initPrograms(){
+func New() shell {
+	s:= shell { c: make(map[string]func([]string)), pc: make(map[string]string) }
+
+	// Load builtins
+	s.c["echo"] = s.echo
+	s.c["exit"] = s.exit
+	s.c["type"] = s.getType
+
+	// Load programs from PATH
 	dirs := strings.Split(os.Getenv("PATH"), ":")
 	for _, dir := range dirs {
 		files, _ := os.ReadDir(dir)
@@ -50,22 +56,5 @@ func (s *shell) initPrograms(){
 			s.pc[file.Name()] = dir
 		}
 	}
-}
-
-func (s shell) Execute(command string, input []string) {
-	if !s.isBuiltin(command) {
-		fmt.Println(command + ": command not found")
-		return
-	}
-	s.c[command](input)
-}
-
-func New() shell {
-	s:= shell { c: make(map[string]func([]string)), pc: make(map[string]string) }
-	s.c["echo"] = s.echo
-	s.c["exit"] = s.exit
-	s.c["type"] = s.getType
-	s.initPrograms()
 	return s
 }
-
